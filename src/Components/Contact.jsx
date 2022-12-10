4149654ab1cb9fa9ba0a584758e78dba6fa5617c
aipoh ruth <aipohruth@gmail.com>
2022-12-10 20:43:21 +0100

    fix: a bug

--- src/Components/Contact.jsx
+++ src/Components/Contact.jsx
@@ -2,6 +2,10 @@ import React from 'react';
 
 
 const Contact = () => {
+    const handleSubmit = (e) =>{
+        e.preventDefault();
+        e.target.reset();
+      }
   return (
     <div id='contact' className='max-w-[1200px] m-auto p-4 my-9'>
         <div className='grid grid-rows-1 md:grid-cols-2 gap-4 mt-4'>
@@ -10,13 +14,13 @@ const Contact = () => {
             </div>
             <div>
             <h2 className='text-center text-xl text-gray-700 font-bold my-4'>Contact Us</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                     
                 <div className='grid grid-cols-1 px-6'>
-                    <input className='border border-gray-600 px-6 my-2 py-2 ' type="text" placeholder='Name' />
-                    <input  className='border border-gray-600 px-6 my-2 py-2 '  type="email" placeholder='Email' />
-                    <input  className='border border-gray-600 px-6 my-2 py-2'  type="number" placeholder='Contact No' />
-                    <textarea  className='border border-gray-600 px-2 my-2 py-2 ' cols="5" rows="5" placeholder='Message ..'></textarea>
+                    <input className='border border-gray-600 px-6 my-2 py-2 ' required type="text" placeholder='Name' />
+                    <input  className='border border-gray-600 px-6 my-2 py-2 ' required  type="email" placeholder='Email' />
+                    <input  className='border border-gray-600 px-6 my-2 py-2' required  type="number" placeholder='Contact No' />
+                    <textarea  className='border border-gray-600 px-2 my-2 py-2 ' required cols="5" rows="5" placeholder='Message ..'></textarea>
                     <button className='mt-2' >Send Message</button>
                 </div>
             </form>

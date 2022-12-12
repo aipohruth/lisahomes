ba418c5af5d89a3f5fc966f142d25059916e02b8
Aipoh Ruth <82672890+aipohruth@users.noreply.github.com>
2022-12-12 15:21:53 +0100

    Create Notfound.jsx

---
+++ src/Components/Notfound.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Notfound = () => {
+  return (
+    <div className='flex justify-center mx-auto mt-[10%] font-extrabold'>
+      <h1 className='text-4xl text-gray-700'>NOT FOUND</h1>
+    </div>
+  )
+}
+
+export default Notfound
